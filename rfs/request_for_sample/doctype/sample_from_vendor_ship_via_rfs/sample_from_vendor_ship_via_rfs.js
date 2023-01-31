a65657b5360f018f79717532bc925ee1c915c88e
ashish-greycube <mr.ashish.shah@gmail.com>
2023-01-31 10:08:48 +0530

    fix: from to From

---
+++ rfs/request_for_sample/doctype/sample_from_vendor_ship_via_rfs/sample_from_vendor_ship_via_rfs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sample From Vendor Ship VIA RFS', {
+	// refresh: function(frm) {
+
+	// }
+});
